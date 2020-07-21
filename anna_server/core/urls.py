9fb018416f31e807692a5789e9b0e213799a72bf
Fabio Pacheco <fabio.pacheco@complyadvantage.com>
2020-07-21 20:11:44 +0100

    ........

--- anna_server/core/urls.py
+++ anna_server/core/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('image/<int:line>', views.image, name='image'),
+    path('draw/', views.draw, name='image'),
 ]
--- anna_server/core/views.py
+++ anna_server/core/views.py
@@ -1,12 +1,14 @@
+from datetime import datetime
+
+from django.core.files.base import ContentFile
 from django.http import HttpResponse
+from django.shortcuts import render
 from core.models import Doodle
+
+from base64 import b64decode
 
 
 def index(request):
-    # from datetime import datetime
-
-    # return HttpResponse(int(datetime.now().timestamp()))
-
     doodle = Doodle.objects.order_by("created_at").filter(active=True).last()
     if not doodle:
         return HttpResponse(1)
@@ -18,3 +20,14 @@ def image(request, line):
     start = line * 300
     end = (start + 300) + 1
     return HttpResponse(",".join(doodle.to_epaper_array()[start:end]))
+
+
+def draw(request):
+    if request.method == "POST":
+        print(request.POST["imgBase64"].replace("data:image/png;base64,", ""))
+        image = b64decode(request.POST["imgBase64"].replace("data:image/png;base64,", ""))
+        data = ContentFile(image, name="{}.png".format(datetime.now().strftime("%Y%m%d_%H%M%S")))
+        Doodle.objects.create(owner=request.user, image=data, active=True)
+
+    return render(request, 'drawing.html', {})
+    # return HttpResponse(",".join(doodle.to_epaper_array()[start:end]))
